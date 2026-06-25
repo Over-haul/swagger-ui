b2d3313998abd3db59d0b60c0e8efce6943bb70b
Rafael Gibim <9031589+Drowze@users.noreply.github.com>
2026-06-25 16:42:19 -0300

    Add TopBarWithEnvSwitchingPlugin

--- src/oh/index.js
+++ src/oh/index.js
@@ -1,8 +1,10 @@
 import OperationPermissionsPlugin from "./plugins/operation-permissions"
+import TopBarWithEnvSwitchingPlugin from "./plugins/top-bar-with-env-switching"
 
 const Oh = {
   plugins: {
     OperationPermissionsPlugin,
+    TopBarWithEnvSwitchingPlugin,
   },
 }
 
---
+++ src/oh/plugins/top-bar-with-env-switching/index.js
@@ -0,0 +1,10 @@
+/**
+ * @prettier
+ */
+import TopBarWithEnvSwitching from "./components/TopBarWithEnvSwitching"
+
+const TopBarWithEnvSwitchingPlugin = () => ({
+  components: { Topbar: TopBarWithEnvSwitching },
+})
+
+export default TopBarWithEnvSwitchingPlugin
